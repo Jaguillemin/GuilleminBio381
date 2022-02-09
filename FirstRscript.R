ec06d67ed8622cb4cf74f6f090b3d95d88391f79
Jaguillemin <jguillem@uvm.edu>
2022-02-09 13:27:20 -0500

    updating homework 3

---
+++ FirstRscript.R
@@ -0,0 +1,11 @@
+#description of script content
+#2/7/22
+#Modified 2/7/22
+#JAG
+rainfall<-runif(10)#Total annual Rainfall
+biomass<-runif(10)
+head(biomass)
+print(rainfall)
+rainfall
+head(rainfall)
+plot(x=rainfall,y=biomass)
